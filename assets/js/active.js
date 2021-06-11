a820037dec02af46f60400b99c61cf2ae88a104f
Amanda Natallie <amanda.natallie.2@gmail.com>
2021-06-11 12:16:18 -0300

    refactor unused code

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,37 +1,11 @@
-// Index of jQuery Active Code
-
-// :: 1.0 PRELOADER ACTIVE CODE
-// :: 2.0 NAVIGATION MENU ACTIVE CODE
-// :: 3.0 SCROLL TO TOP ACTIVE CODE
-// :: 4.0 SCROLL LINK ACTIVE CODE
-// :: 5.0 SMOOTH SCROLLING ACTIVE CODE
-// :: 6.0 AOS ACTIVE CODE
-// :: 7.0 AOS ACTIVE CODE
-// :: 8.0 PREVENT DEFAULT ACTIVE CODE
-// :: 9.0 COUNTERUP ACTIVE CODE
-// :: 10.0 FANCYBOX VIDEO POPUP ACTIVE CODE
-// :: 11.0 FEATURES SLIDER ACTIVE CODE
-// :: 12.0 TESTIMONIALS ACTIVE CODE
-// :: 13.0 APP SCREENSHOTS ACTIVE CODE
-// :: 14.0 CONTACT FORM ACTIVE CODE
-
 (function ($) {
     'use strict';
 
     var $window = $(window);
     var zero = 0;
 
-    // :: 1.0 PRELOADER ACTIVE CODE
-    $(window).on("load", function () {
-        $('.preloader-wapper').addClass('loaded');
-        if ($('.preloader-wapper').hasClass('loaded')) {
-            $('.preloader-main').delay(1200).queue(function () {
-                $(this).remove();
-            });
-        }
-    });
 
-    // :: 2.0 NAVIGATION MENU ACTIVE CODE
+    // :: NAVIGATION MENU ACTIVE CODE
     // dropdown for mobile
     $(document).ready(function () {
         checkWidth(true);
@@ -88,7 +62,7 @@
     }
     navMenu();
 
-    // :: 3.0 SCROLL TO TOP ACTIVE CODE
+    // :: SCROLL TO TOP ACTIVE CODE
     var offset = 300;
     var duration = 500;
 
@@ -106,10 +80,10 @@
         }, duration);
     });
 
-    // :: 4.0 SCROLL LINK ACTIVE CODE
+    // :: SCROLL LINK ACTIVE CODE
     var scrollLink = $('.scroll');
 
-    // :: 5.0 SMOOTH SCROLLING ACTIVE CODE
+    // :: SMOOTH SCROLLING ACTIVE CODE
     scrollLink.on('click', function (e) {
         e.preventDefault();
         $('body,html').animate({
@@ -117,10 +91,10 @@
         }, 1000);
     });
 
-    // :: 6.0 AOS ACTIVE CODE
+    // :: AOS ACTIVE CODE
     AOS.init();
 
-    // :: 7.0 WOW ACTIVE CODE
+    // ::WOW ACTIVE CODE
     new WOW().init();
 
     // :: 8.0 PREVENT DEFAULT ACTIVE CODE
@@ -128,91 +102,6 @@
         $.preventDefault();
     });
 
-    // :: 9.0 COUNTERUP ACTIVE CODE
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-    // :: 10.0 FANCYBOX VIDEO POPUP ACTIVE CODE
-    $(".play-btn").fancybox({
-        animationEffect: "zoom-in-out",
-        transitionEffect: "circular",
-        maxWidth: 800,
-        maxHeight: 600,
-        youtube: {
-            controls: 0
-        }
-    });
-
-    // :: 11.0 FEATURES SLIDER ACTIVE CODE
-    $('.features-slider.owl-carousel').owlCarousel({
-        items: 1,
-        loop: true,
-        nav: false,
-        dots: true,
-        smartSpeed: 1500,
-        autoplay: true,
-        autoplayTimeout: 6000,
-        dotsContainer: '.features-content'
-    });
-
-    // :: 12.0 TESTIMONIALS ACTIVE CODE
-    var testimonialSlider = $('.testimonials');
-    testimonialSlider.owlCarousel({
-        items: 1,
-        nav: false,
-        dots: true,
-        smartSpeed: 500,
-        autoplay: true,
-        loop: true,
-        mouseDrag: true,
-        touchDrag: true,
-        responsiveClass: true,
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            768: {
-                items: 1
-            }
-        }
-    });
-
-    testimonialSlider.on("translate.owl.carousel", function () {
-        $(".single-testimonial img, .single-testimonial-thumb img, .client-rating").removeClass("animated zoomIn").css("opacity", "0");
-    });
-    testimonialSlider.on("translated.owl.carousel", function () {
-        $(".single-testimonial img, .single-testimonial-thumb img, .client-rating").addClass("animated zoomIn").css("opacity", "1");
-    });
-
-    // :: 13.0 BRANDING SLIDER ACTIVE CODE
-    $('.branding-slider.owl-carousel').owlCarousel({
-        loop: true,
-        margin: 20,
-        nav: false,
-        dots: false,
-        smartSpeed: 2000,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        responsive: {
-            0: {
-                items: 2
-            },
-            576: {
-                items: 3
-            },
-            768: {
-                items: 4
-            },
-            992: {
-                items: 5
-            }
-        }
-    });
 
     // :: 14.0 APP SCREENSHOTS ACTIVE CODE
     $('.app-screenshots').slick({
@@ -250,48 +139,6 @@
                 }
             }
           ]
-    });
-
-    // :: 15.0 CONTACT FORM ACTIVE CODE
-    // Get the form.
-    var form = $('#contact-form');
-    // Get the messages div.
-    var formMessages = $('.form-message');
-    // Set up an event listener for the contact form.
-    $(form).submit(function (e) {
-        // Stop the browser from submitting the form.
-        e.preventDefault();
-        // Serialize the form data.
-        var formData = $(form).serialize();
-        // Submit the form using AJAX.
-        $.ajax({
-                type: 'POST',
-                url: $(form).attr('action'),
-                data: formData
-            })
-            .done(function (response) {
-                // Make sure that the formMessages div has the 'success' class.
-                $(formMessages).removeClass('error');
-                $(formMessages).addClass('success');
-
-                // Set the message text.
-                $(formMessages).text(response);
-
-                // Clear the form.
-                $('#contact-form input,#contact-form textarea').val('');
-            })
-            .fail(function (data) {
-                // Make sure that the formMessages div has the 'error' class.
-                $(formMessages).removeClass('success');
-                $(formMessages).addClass('error');
-
-                // Set the message text.
-                if (data.responseText !== '') {
-                    $(formMessages).text(data.responseText);
-                } else {
-                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                }
-            });
     });
 
 }(jQuery));
